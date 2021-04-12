9c4a99de1d41d57157246cada4dcdf4ad9607ee5
Xinyu <xyfei27@gmail.com>
2021-04-12 16:16:50 -0400

    add comments

--- test_main.py
+++ test_main.py
@@ -2,6 +2,7 @@ from import_data import read_data
 import networkx as nx
 
 if __name__ == '__main__':
+    # create graph (g) and terminal list (t_list) from the data file.
     g, t_list = read_data("data/B/b01.stp")
     # read_data("data/ES10FST/es10fst01.stp")
     print(list(g.nodes))
