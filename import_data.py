98dcfbfe7953d9eb742cd7145331894c9b297552
Xinyu <xyfei27@gmail.com>
2021-04-12 20:21:00 -0400

    add mode in read data for steiner forest

--- import_data.py
+++ import_data.py
@@ -23,5 +23,15 @@ def read_data(file_name, mode = 'tree'):
             t_list.append(int(data[1]))
 
     # print(list(g.nodes), list(g.edges), t_list)
+    if mode == 'tree':
+        return g, t_list
 
-    return g, t_list
+    pair_list = []
+    if mode == 'forest':
+        i = 0
+        while i + 1 < len(t_list):
+            s = t_list[i]
+            t = t_list[i + 1]
+            pair_list.append((s, t))
+            i = i + 2
+        return g, pair_list
--- test_main.py
+++ test_main.py
@@ -3,11 +3,13 @@ import networkx as nx
 
 if __name__ == '__main__':
     # create graph (g) and terminal list (t_list) from the data file.
-    g, t_list = read_data("data/B/b01.stp")
+    g, t_list = read_data("data/B/b01.stp", mode='tree')
+    g, pair_list = read_data("data/B/b01.stp", mode="forest")
     # read_data("data/ES10FST/es10fst01.stp")
     print(list(g.nodes))
     for (u, v, wt) in g.edges.data('weight'):
         print((u, v, wt))
     print(t_list)
+    print(pair_list)
     print(nx.shortest_path(g, source=1, target=47, weight='weight'),
           nx.shortest_path_length(g, source=1, target=47, weight='weight'))
